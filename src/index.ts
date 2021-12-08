05d3b189d1e01c40dd27535cd10c670e562bb62b
cesarzxk <43748428+cesarzxk@users.noreply.github.com>
2021-12-08 14:32:08 -0300

    Implementação - Servidor socket io com Express
    Foi implementado a entrada em salas e o envio de mensagens para os integrantes das salas

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import express from 'express';
+import http from 'http';
+import { Server, Socket} from "socket.io";
+const app = express()
+const server = http.createServer(app);
+const io = new Server(server);
+
+io.on('connection', (socket)=>{
+    socket.on('join',(room)=>{
+        socket.join(room.code)
+        socket.emit('statusJoin', {room:room.code, userId:room.id})
+    })
+
+    socket.on('chat', ({message, id, date, server})=>{
+        console.log(message);
+        socket.to(server).emit('messages', {message, id, date})
+    })
+})
+
+io.on('disconnect', ()=>{
+    console.log('usuario desconectado')
+})
+
+export const rooms = ()=>{}
+
+server.listen(3333, () =>{
+    console.log('Servidor iniciado! 😁')
+})
+
